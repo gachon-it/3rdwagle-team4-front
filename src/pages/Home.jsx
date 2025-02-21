bcde8357208dbaca4534b20911d42b9e5b7869e9
junwoo <kjw4523705@naver.com>
2025-02-21 21:31:40 +0900

    4th  commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
+import HomeMenu from "../components/Profile/Homemenu"
 import {  getCharacterImage } from "../util/get-character-image"
 
 export default function Home() {
+    const nickname = "습관이"
     const day = 30
     const emotionId = 1
+    const level = 5
+    const xp = 190
+    const maxXp = 500
+    const progress = (xp / maxXp) * 100
     return (
     <div className="Home">
         <div className="home-header">
@@ -10,8 +16,23 @@ export default function Home() {
         </div>
         <div className="home-character">
             <img src={getCharacterImage(emotionId)}/>
+            <h4>{nickname}</h4>
+        </div>
+
+        <div className="home-character-info">
+            <div className="level">레벨 {level}</div>
+            <div className="xp">경험치 {xp}</div>
+            <div className="xp-bar-container">
+        
+        <div className="xp-bar" style={{ width: `${progress}%` }}></div>
         </div>
         
+        <div className="xp-percent">{progress.toFixed(0)}%</div>
+        </div>
+        <div className="home-habbit-list">
+            
+        </div>
+        <HomeMenu />
     </div>
     )
 }
